c8e2d096b8e57d1d811c000a800f1572010197b0
Samy Ayoub <samyayoub@gmail.com>
2019-03-10 19:20:04 +0000

    Adding Validations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+    validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+    validates :author, presence: true, length: { maxiumum: 50, minimum: 3 }
 end
